1a30cb32b24dc3efebbd8c4ac251bad4cc1ec978
zhuhui <zhuhui@454915f7-84fc-e311-8368-000c29c90465>
2017-06-27 10:09:42 +0000

    微信开发 时间银行

--- Addons/TimeBank/uninstall.sql
+++ Addons/TimeBank/uninstall.sql
@@ -1,3 +1,6 @@
 DELETE FROM `wp_attribute` WHERE `model_name`='time_bank';
 DELETE FROM `wp_model` WHERE `name`='time_bank' ORDER BY id DESC LIMIT 1;
 DROP TABLE IF EXISTS `wp_time_bank`;
+DELETE FROM `wp_attribute` WHERE `model_name`='time_deposit';
+DELETE FROM `wp_model` WHERE `name`='time_deposit' ORDER BY id DESC LIMIT 1;
+DROP TABLE IF EXISTS `wp_time_deposit`;
